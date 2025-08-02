a55e77f967e616aee0a8747d0ab810dbb946937c
Regina Angela Ramones <angela17ramones@gmail.com>
2025-08-02 15:56:09 +0800

    Update Book.cs

--- Models/Book.cs
+++ Models/Book.cs
@@ -3,10 +3,13 @@
     public class Book
     {
         public int AuthorId { get; set; }
+        
         public string Title { get; set; }
+        
         public int ISBN { get; set; }
 
         public Author Writer { get; set; }
     }
 }
+
 
